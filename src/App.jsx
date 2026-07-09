e8786523e5bfb3d7d7d2307ae5e63147fdab853c
Musa Abdusamed? <musaabdusamed@gmail.com?>
2026-07-09 08:10:53 -1200

    Add Profile Cards project

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
 import profileImage from "./assets/profile.png";
-import profile1 from "./assets/profile1.jpg"
-import profile2 from "./assets/profile2.jpg"
-import profile3 from "./assets/profile3.jpg"
-// function ProfileCard(){
-//   return(
-//     <div className="max-w-sm round-xl bg-white p-6 shadow-lg">
-//       <img src={profileImage}
-//       alt="my profile"
-//       className="mx-outo h-10 w-10 round-full object-cover" />
+import profile1 from "./assets/profile1.jpg";
+import profile2 from "./assets/profile2.jpg";
+import profile3 from "./assets/profile3.jpg";
 
-//     </div>
-//   );
-// }
-// function App() {
-//   return (
-  
-//     <div className="min-h-screen flex items-center justify-center bg-slate-1">
-      
-//       <div className="w-96 rounded-xl bg-white p-8 shadow-xl">
-//         <h1 className="text-3xl font-bold text-blue-600">
-//           Welcome, Musa!
-//         </h1>
-// <ProfileCard/>
-//         <p className="mt-3 text-gray-red">
-//           React and Tailwind are working together.
-//         </p>
-
-//         <button className="mt-6 rounded-lg bg-blue-600 px-4 
-//         py-2 text-white transition hover:bg-blue-700">
-//           Start Learning
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
 function ProfileCard({name,job,image,location}){
   return(
     <div className="w-72 rounded-xl bg-white p-6 shadow-lg">
@@ -56,27 +25,6 @@ function ProfileCard({name,job,image,location}){
     
   );
 }
-// function App(){
-//   return(
-//     <div className="flex gap-6">
-// <ProfileCard 
-// name="Musa Abdusamed"
-// job="Frontend Developer"
-// image={profileImage}
-// />
-// <ProfileCard 
-// name="ahmed"
-// job="Backend Developer"
-// image={profileImage}
-// />
-// <ProfileCard 
-// name="ali"
-// job="UI Designer"
-// image={profileImage}
-// />
-//     </div>
-//   );
-// }
 
 
 const users=[{
@@ -119,5 +67,4 @@ function App(){
 }
 
 
-
-export default App;
+ export default App;
